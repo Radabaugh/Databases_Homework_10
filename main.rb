52d315372898e84568bbd8e5e79f8fc9c39e45e0
Tyler <radabaughtyler24@gmail.com>
2018-03-14 21:51:37 -0700

    redis add and delete key

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 # NoSQL with Redis
 # Tyler Radabaugh
+
+require 'redis'
+
+redis = Redis.new
+
+redis.set('Todd', 'Beagle')
+value = redis.get('Todd')
+puts value
+redis.del('Todd')
+value = redis.get('Todd')
+puts value
